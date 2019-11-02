bf980a1f1d4080162441a6fd00213c077a68a3f0
honora hegarty <honora.hegarty@verizon.net>
2019-11-01 22:54:26 -0400

    updating variables

--- Assets/Javascript/game.js
+++ Assets/Javascript/game.js
@@ -1,7 +1,23 @@
+$(document).ready(function(){
+
+var rand = [];
+
+for (var r = 19; r < 121; r++) {
+    rand.push(r);
+}
+
+var crystals = [];
+
+for (var c = 1; c <13; c++){
+    crystals.push(c);
+}
+
+var randNumber;
+var crystalNumber =[];
+var c1;
+var c2;
+var c3;
+var c4;
+var totalScore = 0;
 var wins = 0;
-var losses =0;
-var totalscore
-var crystal1
-var crystal2
-var crystal3
-var crystal4
+var losses= 0;
